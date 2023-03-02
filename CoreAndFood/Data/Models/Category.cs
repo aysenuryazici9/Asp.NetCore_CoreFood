276741a93b983c54ee830d05bce5c2053ec5e5f3
aysenuryazici9 <aysnryzc129@gmail.com>
2023-03-03 00:17:26 +0300

    Asp.NetCore_CoreFood
    Asp.NetCore_CoreFood

---
+++ CoreAndFood/Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Data.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        [Required(ErrorMessage ="Category Name Not Empty!")]
+        [StringLength(20, ErrorMessage ="Please only enter 4-20 characters!",MinimumLength=5)]
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool Status { get; set; }
+        public List<Food> Foods { get; set; }
+    }
+}
---
+++ CoreAndFood/Migrations/20230112212022_doubleprice.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreAndFood.Migrations
+{
+    public partial class doubleprice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Foods",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Foods",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double));
+        }
+    }
+}
